901380eb98152d7ed56f4f4d68531e42a5304fdb
NatocaOca <neocana@misena.edu.co>
2019-09-03 14:33:52 -0500

    Agregar archivos de proyecto.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+   public class Calcular
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int Sumar(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+
+        public int Restar(int valor1, int valor2)
+        {
+            return valor1 - valor2;
+        }
+    }
+}
---
+++ CalculadoraTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CalculadoraTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Sumar_UnoMasDos_Test()
+        {
+            var calculadora = new Calculadora.Calcular();
+            int resulatdoesperado = 3;
+            int resultado = calculadora.Sumar(1, 2);
+            Assert.AreEqual(resulatdoesperado, resultado);
+
+        }
+
+        [Test]
+        public void Restar_TresMenosDos_Test()
+        {
+            var calculadora = new Calculadora.Calcular();
+            int resultadoE = 1;
+            int resultado = calculadora.Restar(3, 2);
+            Assert.AreEqual(resultadoE, resultado);
+        }
+    }
+}
